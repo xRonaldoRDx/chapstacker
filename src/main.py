5825d8dfe9b570886d4dce94b705f1defb5fa108
Ronaldo Rossetti <ronaldord.3001@gmail.com>
2026-01-14 15:26:24 -0300

    feat: implement volume identification and grouping logic

--- src/main.py
+++ src/main.py
@@ -4,6 +4,33 @@ validated_path = utils.user_path_validation() # --- Solicita ao usuário o camin
 
 origin_chapters = utils.get_chapters_list(validated_path) # --- Insere todos os capítulos encontrados em um vetor.
 
+
+
 # --- Lista todos os capítulos encontrados presentes no origin_chapters.
 print(f"\nForam encontrados {len(origin_chapters)} capítulos:")
 print("\n".join(origin_chapters))
+
+
+
+# --- Verifica o nome de cada pasta de capítulo em busca do número de volume e então a anexa num padrão chave(volume) e valor(capítulo)
+volumes = {} 
+
+# --- Verifica cada capítulo encontrado
+for chapter_name in origin_chapters:
+    num_vol = utils.get_volume_number(chapter_name)
+
+    if num_vol is not None:
+        # Se a a chave do volume não existir, é criada.
+        if num_vol not in volumes:
+            volumes[num_vol] = [] 
+
+        # Atribui o caítulo na chave certa
+        volumes[num_vol].append(chapter_name)
+
+
+# --- Exibe os volumes identificados e seus respectivos capítulos
+print(f'\nOs capítulos indicados foram organizados em {len(volumes)} volumes da seguinte forma:')
+for volume, chapters in volumes.items():
+    print(f'\nVolume {volume}:')
+    for chapter in chapters:
+        print(f'\t{chapter}')
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 def is_valid_directory(inserted_path):
     return inserted_path.exists() and inserted_path.is_dir()
@@ -8,10 +9,10 @@ def user_path_validation():
         input_path = Path(input('Digite o caminho da pasta dos capítulos: '))
 
         if is_valid_directory(input_path):
-            print(f'O caminho digitado é válido. Iremos trabalhar na pasta {input_path}.')
+            print(f'\nO caminho digitado é válido. Iremos trabalhar na pasta {input_path}.')
             return input_path
         else:
-            print('O caminho digitado não é válido, verifique a escrita e tente novamente.')
+            print('O caminho digitado não é válido, verifique a escrita e tente novamente.\n')
 
 def get_chapters_list(validated_path):
 
@@ -22,3 +23,19 @@ def get_chapters_list(validated_path):
     
     chapters.sort()    
     return chapters
+
+
+def get_volume_number(chapter_name):
+    # (?i) = ignorar maiúsculas/minúsculas
+    # (?:vol|v|volume) = procurar por uma dessas palavras
+    # [\s.]* = aceitar espaços ou pontos no meio
+    # (\d+) = capturar um ou mais números
+    pattern = r'(?i)(?:volume|vol|v)[\s.]*(\d+)'
+
+    match = re.search(pattern, chapter_name)
+
+    if match:
+        # --- Recebe apenas o que estava dentro do parênteses (\d+)
+        volume_number = match.group(1) 
+        return int(volume_number) # Transforma "01" em "1"
+    return None
